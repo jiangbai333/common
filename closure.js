625e7edf40e2d7263903f705c04635b8addcd037
b-jiang <jiangbai333@gmail.com>
2016-02-18 09:03:23 +0800

    Update closure.js

--- closure.js
+++ closure.js
@@ -2,11 +2,14 @@
  *		利用闭包实现的数据存储
  * @param string key 键名
  * @param mixed value 键值
+ * @author 姜柏超
+ * @time 2016-02-18 08:46:29
  */
 var db = (function() {
     
-    var data = {};
+    var data = {}; //数据容器对象
     
+    /**对发布暴露 getter、setter 接口*/
     return function(key, value) {
         return value === undefined ? data[key] : data[key] = value;
     }
