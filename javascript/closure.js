5b1562e9ec3ee2cb89902fa507699480351f9f60
unknown <jiangbai333@163.com>
2016-02-19 11:27:03 +0800

    optimization closure.js/db

--- javascript/closure.js
+++ javascript/closure.js
@@ -15,3 +15,67 @@ var db = (function() {
     }
     
 })();
+
+/**
+ *		进一步优化的数据存储功能
+ * @author 姜柏超
+ * @time 2016-02-19 11:17:29
+ */
+var db = (function() {
+    
+    var data = {}; //数据容器对象
+    
+	/**
+	 *		验证键名是否是合适的数据类型
+	 * @param mixed param 将要验证的键名
+	 * @throws msg
+	 * @return boolean
+	 */
+    function verification(param) {
+        try {
+			/**判断键名是否为字符串*/
+            if ( "string" != typeof( param ) ) {
+                throw "键名必须为字符串格式，不允许其它类型数据作为键名";
+            }
+        } catch( e ) {
+
+            console.log(e);
+            return false;
+        }
+
+        return true;
+    }
+    
+	/**返回一个可对数据容器进行操作的对象*/
+    return new function() {
+                
+		/**
+		 *		读取数据
+		 * @param string key 将要读取的数据对应的键名
+		 * @return mixed 键值
+		 */
+        this.get = function(key) {
+            return data[key];
+        }
+		
+		/**
+		 *		插入数据
+		 * @param string key 将要插入的数据对应的键名
+		 * @param mixed value 将要插入的数据
+		 * @return mixed 若插入数据成功则返回被插入的数据,否则返回 false
+		 */
+        this.add = function(key, value) {
+            return verification(key) ? data[key] = value : false;
+        }
+
+		/**
+		 *		移除数据
+		 * @param string key 将要移除的数据对应的键名
+		 */
+        this.del = function(key) {
+            delete( data[key] );
+        }
+
+    }
+    
+})();
