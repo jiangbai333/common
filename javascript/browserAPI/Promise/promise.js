9dd99dc90e7494d1167fa19ed63081ed3765ac17
unknown <jiangbai333@163.com>
2016-03-25 11:07:01 +0800

    change promise.js

--- javascript/browserAPI/Promise/promise.js
+++ javascript/browserAPI/Promise/promise.js
@@ -6,18 +6,19 @@ var messageBox = document.querySelector("#msg");
  * 创建一个用于异步回调的 Promise 对象
  */
 var pro = new Promise(function(resolve, reject) {
-	/** resolve, reject作为两个不同场景下的回调函数被传入 */
+	/** resolve, reject 作为两个不同场景下的回调函数被传入 */
+	/** then 方法通过Promise内回调函数的执行逻辑, 若执行 resolve 则 then 方法默认执行第一个参数对应的回调函数,否则 then 方法执行第二个参数对应的回调函数*/
+	
 	setTimeout(function () {
 		messageBox.innerHTML += "Promise message<br>";
-		resolve("Promise resolve");
+		//resolve("Promise resolve");
+		reject("Promise reject");
 	}, 1000);
 
-}).then(function(resolve, reject) {
-	/** resolve, reject作为两个不同场景下的回调函数被传入 */
-	messageBox.innerHTML += "write " + resolve + " message<br>";
-},function(resolve, reject){
-	/** resolve, reject作为两个不同场景下的回调函数被传入 */
-	messageBox.innerHTML += "write " + reject + " message<br>";
+}).then(function(res) {
+	messageBox.innerHTML += "write " + res + " message<br>";
+},function(rej){
+	messageBox.innerHTML += "write " + rej + " message<br>";
 });
 
 messageBox.innerHTML += "write message<br>";
