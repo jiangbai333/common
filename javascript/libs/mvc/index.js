a8b347c3702b59928c62151df354bca6d37b6218
unknown <jiangbai333@163.com>
2016-03-29 16:18:38 +0800

    add javascript mvc

---
+++ javascript/libs/mvc/index.js
@@ -0,0 +1,34 @@
+function Model(value) {
+	this._value = typeof value === 'undefined' ? '' : value;
+	this._listeners = [];
+}
+
+Model.prototype.set = function (value) {
+	var self = this;
+	self._value = value;
+	setTimeout(function () {
+		self._listeners.forEach(function (listener) {
+			listener.call(self, value);
+		});
+	});
+};
+
+Model.prototype.watch = function (listener) {
+	this._listeners.push(listener);
+};
+
+Model.prototype.bind = function (node) {
+	this.watch(function (value) {
+		node.innerHTML = value;
+	});
+};
+
+function Controller(callback) {
+	var models = {};
+	var views = Array.prototype.slice.call(document.querySelectorAll('[bind]'), 0);
+	views.forEach(function (view) {
+		var modelName = view.getAttribute('bind');
+		(models[modelName] = models[modelName] || new Model()).bind(view);
+	});
+	callback.call(this, models);
+}
